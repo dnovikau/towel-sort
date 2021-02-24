477ce5b619c89cf5bd6bef24dfbf1c33a04636a0
Daniil Novikau <novikaud@gmail.com>
2021-02-24 12:30:37 +0300

    Task: https://github.com/rolling-scopes-school/towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const asc = (a, b) => a - b;
+  const desc = (a, b) => b - a;
+
+  const result = [];
+  return matrix && matrix.length ? matrix.reduce((result, line, index) => {
+    const isOdd = index > 0 && !(index % 2 === 0);
+    result.push(...line.sort(isOdd ? desc : asc));
+
+    return result;
+  }, []) : [];
+
 }
